6b678fe9d58fb735049f1fc6257302ed3dd50a03
guptakrati0899 <87761763+guptakrati0899@users.noreply.github.com>
2021-09-28 18:44:59 +0530

    [Krati]Refactored: Refactored JS Conversion code For Quantity Measurement

--- js/tempConverter.js
+++ js/tempConverter.js
@@ -4,13 +4,19 @@ let inputType = document.getElementById('inputType');
 let resultType = document.getElementById('resultType');
 let inputTypeValue,resultTypeValue;
 let result , result1;
+let inputTypeValue1, resultTypeValue1;
 
 
-window.addEventListener("keyup",myResult);
-inputType.addEventListener("change",myResult);
-resultType.addEventListener("change",myResult);
-inputTypeValue = inputType.value;
-resultTypeValue = resultType.value;
+userInput.addEventListener("keyup", myResult);
+userResult.addEventListener("keyup", myResult1);
+let input;
+if (input = userInput) {
+        inputType.addEventListener("change", myResult);
+        resultType.addEventListener("change", myResult1);
+} else {
+        inputType.addEventListener("change", myResult);
+        resultType.addEventListener("change", myResult1);
+}
 
 
 
@@ -18,6 +24,57 @@ function unitMultiply(input1,input2){
     let answer = input1 * input2;;
      return answer;
  }
+
+
+ 
+function myResult1() {
+
+        inputTypeValue1 = resultType.value;
+        resultTypeValue1 = inputType.value;
+        let option1 = inputTypeValue1 + "-" + resultTypeValue1;
+
+        switch(option1){
+                case "celsius-fahrenheit" :              
+                        result =  unitMultiply(userResult.value,1.8);
+                        userInput.value = result + 32;
+                        console.log(userInput.value);
+                        break;
+                case "celsius-Kelvin" :
+                        userInput.value = (userResult.value * 1) + 273.15;
+                        console.log(userInput.value);
+                        break;
+                case "celsius-celsius" :
+                        userInput.value = userResult.value ;
+                        console.log(userInput.value);
+                        break;
+                case "fahrenheit-celsius" :
+                        result = userResult.value - 32;
+                        userInput.value =  unitMultiply(result,0.5556);                      
+                        console.log(userInput.value);
+                        break;
+                case "fahrenheit-Kelvin":
+                        userInput.value = (userResult.value - 32) *( 5/9) + 273.15;
+                        console.log( userInput.value);
+                        break; 
+                case "fahrenheit-fahrenheit":
+                        userInput.value = userResult.value ;
+                        console.log(userInput.value);
+                        break;
+                case "Kelvin-celsius" :
+                        userInput.value = (userResult.value * 1) - 273.15;                        
+                        console.log(userInput.value);
+                        break;
+                case "Kelvin-fahrenheit":
+                        userInput.value = (userResult.value  - 273.15) * (9/5) + 32;
+                        console.log( userInput.value);
+                        break;
+                case "Kelvin-Kelvin":
+                        userInput.value = userResult.value ;
+                        console.log(userInput.value);
+                        break;
+        }
+}        
+
  
 
 
